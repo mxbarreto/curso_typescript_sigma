863c72e5bf0feaeb54112e51f04ada3396967cef
Maxsuel Francisco Barreto <maxsuel.barreto@sigma.com.br>
2018-05-17 13:21:18 -0300

    Aula 14 - Importanto bibliotecas Javascript

--- primeiras_aulas/app.ts
+++ primeiras_aulas/app.ts
@@ -1,14 +1,17 @@
 import { Spacecraft, Containership } from "./base-ships";
 import { MillenniumFalcon } from "./starfighters";
 
+import * as _ from 'lodash'
+console.log(_.pad("Typescripts Examples", 40, "=" ))
+
 
 let shipt = new Spacecraft('hyperdrive')
 shipt.jumIntoHyperspace()
 
 let falcon = new MillenniumFalcon()
-
 falcon.jumIntoHyperspace()
 
 let goodForTheJob = (ship: Containership ) => ship.cargoContainers > 2
-
 console.log(`Is falcon good for the job? ${goodForTheJob ( falcon ) ? 'yes' : 'no' }`)
+
+console.log(_.pad("Fim Typescritps", 40, "="))
--- primeiras_aulas/dist/app.js
+++ primeiras_aulas/dist/app.js
@@ -2,9 +2,12 @@
 exports.__esModule = true;
 var base_ships_1 = require("./base-ships");
 var starfighters_1 = require("./starfighters");
+var _ = require("lodash");
+console.log(_.pad("Typescripts Examples", 40, "="));
 var shipt = new base_ships_1.Spacecraft('hyperdrive');
 shipt.jumIntoHyperspace();
 var falcon = new starfighters_1.MillenniumFalcon();
 falcon.jumIntoHyperspace();
 var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
 console.log("Is falcon good for the job? " + (goodForTheJob(falcon) ? 'yes' : 'no'));
+console.log(_.pad("Fim Typescritps", 40, "="));
